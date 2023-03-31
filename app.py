ca6da86d06c1e8173a570b56fdaba86809c74883
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-03-31 02:46:02 -0300

    refactor functions app.py

--- app.py
+++ app.py
@@ -1,87 +1,92 @@
 import pandas as pd
 import numpy as np
-
-dados_completos_chuvas = pd.read_csv("./csv/chuvas_C_01944009.csv", sep=";",
-                                     encoding='ISO 8859-1', skiprows=12, decimal=",", usecols=["NivelConsistencia", "Data", "Maxima"], index_col=False)
-
-dados_completos_chuvas['Data'] = pd.to_datetime(
-    dados_completos_chuvas['Data'], format='%d/%m/%Y')
-
-dados_completos_chuvas = dados_completos_chuvas.sort_values(
-    by='Data', ascending=True)
-
-print(dados_completos_chuvas)
-
-# Para os Dados Consistidos => NivelConsistência = 2
-
-dados_consistentes = dados_completos_chuvas.loc[dados_completos_chuvas['NivelConsistencia'] == 2, [
-    "Data", "Maxima"]]
-
-start_date = dados_consistentes['Data'].min()
-end_date = dados_consistentes['Data'].max()
-new_date_range = pd.date_range(
-    start=start_date, end=end_date, freq='MS')
-
-new_consist_df = pd.DataFrame({'Data': new_date_range})
-new_consist_df['Maxima_novo'] = ''
-
-dados_consistentes = dados_consistentes.set_index(
-    'Data').reindex(new_date_range)
-new_consist_df = new_consist_df.set_index('Data')
+import pickle
 
 
-dados_consistentes = dados_consistentes.join(
-    new_consist_df, rsuffix='_novo', how='outer')
-dados_consistentes = dados_consistentes.sort_index()
-dados_consistentes = dados_consistentes.reset_index()
-dados_consistentes = dados_consistentes.drop('Maxima_novo', axis='columns')
-dados_consistentes.rename(columns={'index': 'Data'}, inplace=True)
+def load_data(file_path):
+    return pd.read_csv(file_path, sep=";", encoding='ISO 8859-1', skiprows=12, decimal=",", usecols=["NivelConsistencia", "Data", "Maxima"])
 
-# Para os Dados Brutos => NivelConsistência = 1
 
-dados_brutos = dados_completos_chuvas.loc[dados_completos_chuvas['NivelConsistencia'] == 1, [
-    "Data", "Maxima"]]
+def process_data(df):
+    df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y')
+    df = df.sort_values(by='Data', ascending=True)
+    return df
 
-# EXCLUIR PRIMEIROS E ÚLTIMOS DADOS FORA DO CICLO
 
-index_first_sep = dados_consistentes.loc[dados_consistentes['Data'].dt.month == 9].index[0]
-index_last_oct = dados_consistentes.loc[dados_consistentes['Data'].dt.month == 10].index[-1]
-index_last = dados_consistentes.shape[0] - 1
+def get_consistent_data(df):
+    consistent_data = df.loc[df['NivelConsistencia'] == 2, ["Data", "Maxima"]]
+    start_date = consistent_data['Data'].min()
+    end_date = consistent_data['Data'].max()
+    new_date_range = pd.date_range(start=start_date, end=end_date, freq='MS')
+    consistent_data = consistent_data.set_index('Data').reindex(new_date_range)
+    consistent_data = consistent_data.sort_index(
+    ).reset_index().rename(columns={'index': 'Data'})
+    return consistent_data
 
-inicial_drop_range = dados_consistentes.iloc[0:index_first_sep+1]
-final_drop_range = dados_consistentes.iloc[index_last_oct:index_last + 1]
 
-dados_consistentes = dados_consistentes.drop(inicial_drop_range.index)
-dados_consistentes = dados_consistentes.drop(final_drop_range.index)
+def get_raw_data(df):
+    return df.loc[df['NivelConsistencia'] == 1, ["Data", "Maxima"]].reset_index(drop=True)
 
-dados_consistentes = dados_consistentes.reset_index(drop=True)
-dados_brutos = dados_brutos.reset_index(drop=True)
 
-# PREENCHER VALORES VAZIOS DE MAXIMA COM OS DADOS BRUTOS
+def remove_out_of_cycle_data(df):
+    index_first_sep = df.loc[df['Data'].dt.month == 9].index[0]
+    index_last_oct = df.loc[df['Data'].dt.month == 10].index[-1]
+    index_last = df.shape[0] - 1
 
-dados_consistentes.set_index('Data', inplace=True)
-dados_brutos.set_index('Data', inplace=True)
+    inicial_drop_range = df.iloc[0:index_first_sep+1]
+    final_drop_range = df.iloc[index_last_oct:index_last + 1]
 
-merged_df = dados_consistentes.merge(
-    dados_brutos, left_index=True, right_index=True, how='left')
+    df = df.drop(inicial_drop_range.index)
+    df = df.drop(final_drop_range.index)
 
-merged_df['Maxima_x'].fillna(merged_df['Maxima_y'], inplace=True)
+    return df.reset_index(drop=True)
 
-merged_df.drop('Maxima_y', axis=1, inplace=True)
 
-merged_df.rename(columns={'Maxima_x': 'Maxima'}, inplace=True)
+def merge_and_fill_data(consistent_data, raw_data):
+    merged_df = consistent_data.merge(
+        raw_data, left_index=True, right_index=True, how='left', suffixes=('', '_y'))
+    merged_df['Maxima'].fillna(merged_df['Maxima_y'], inplace=True)
+    merged_df.drop('Maxima_y', axis=1, inplace=True)
+    return merged_df.reset_index(drop=True)
 
-merged_df.reset_index(inplace=True)
 
-# MÁXIMA E LN DA MAXIMA PARA CADA ANO HIDROLÓGICO
+def add_hydrological_year(df):
+    df["AnoHidrologico"] = df["Data"].apply(
+        lambda x: x.year if x.month >= 10 else x.year - 1)
+    ano_hidrologico_df = df.groupby("AnoHidrologico")["Maxima"].max(
+    ).reset_index().rename(columns={"Maxima": "Pmax_anual"})
+    ano_hidrologico_df['ln_Pmax_anual'] = np.log(
+        ano_hidrologico_df['Pmax_anual'])
+    return ano_hidrologico_df
 
-merged_df["AnoHidrologico"] = merged_df["Data"].apply(
-    lambda x: x.year if x.month >= 10 else x.year - 1)
 
-ano_hidrologico_df = merged_df.groupby("AnoHidrologico")[
-    "Maxima"].max().reset_index()
+def save_data(data, file_path):
+    with open(file_path, 'wb') as f:
+        pickle.dump(data, f)
 
-ano_hidrologico_df = ano_hidrologico_df.rename(
-    columns={"Maxima": "Pmax_anual"})
 
-ano_hidrologico_df['ln_Pmax_anual'] = np.log(ano_hidrologico_df['Pmax_anual'])
+def main():
+    input_file_path = "./csv/chuvas_C_01944009.csv"
+    rain_data = load_data(input_file_path)
+    rain_data = process_data(rain_data)
+
+    consistent_rain_data = get_consistent_data(rain_data)
+    raw_rain_data = get_raw_data(rain_data)
+
+    consistent_rain_data = remove_out_of_cycle_data(consistent_rain_data)
+
+    filled_rain_data = merge_and_fill_data(consistent_rain_data, raw_rain_data)
+
+    hydrological_year_data = add_hydrological_year(filled_rain_data)
+
+    print(hydrological_year_data)
+
+    output_file_path = "hydrological_year_data.pkl"
+    save_data(hydrological_year_data, output_file_path)
+
+    consistent_rain_data.to_csv('./csv/consistent_rain_data.csv', sep=',')
+    raw_rain_data.to_csv('./csv/raw_rain_data.csv', sep=',')
+
+
+if __name__ == "__main__":
+    main()
