949a8b7444137cb8f08324a4e97848bcd1d2f9cc
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-04-17 20:23:43 -0300

    feat: export results in json

--- python_app/app.py
+++ python_app/app.py
@@ -3,6 +3,8 @@ import pandas as pd
 from process_data import main as process_data
 from teste_outlier import main as teste_outlier
 from distributions import main as distributions
+from k_coefficient import main as k_coefficient
+from disaggregation_coef import main as disaggregation_coef
 from ventechow import main as ventechow
 
 
@@ -11,29 +13,40 @@ def load_data():
     input_data = pd.read_csv(input_file_path, sep=";", encoding='ISO 8859-1', skiprows=12,
                              decimal=",", usecols=["NivelConsistencia", "Data", "Maxima"], index_col=False)
 
-    teste_gb_file_path = "python_app/csv/Tabela_Teste_GB.csv"
-    teste_gb = pd.read_csv(teste_gb_file_path, sep=",",
-                           encoding='ISO 8859-1', decimal=",", index_col=False)
+    gb_test_file_path = "python_app/csv/Tabela_Teste_GB.csv"
+    gb_test = pd.read_csv(gb_test_file_path, sep=",",
+                          encoding='ISO 8859-1', decimal=",", index_col=False)
 
     yn_sigman_file_path = "python_app/csv/Tabela_YN_sigmaN.csv"
     table_yn_sigman = pd.read_csv(yn_sigman_file_path, sep=",", encoding='ISO 8859-1',
-                             decimal=",", usecols=["Size", "YN", "sigmaN"], index_col=False)
-    
-    return input_data, teste_gb, table_yn_sigman
+                                  decimal=",", usecols=["Size", "YN", "sigmaN"], index_col=False)
+
+    return input_data, gb_test, table_yn_sigman
+
 
 
 def main():
-    raw_df, teste_gb, table_yn_sigman = load_data()
+    raw_df, gb_test, table_yn_sigman = load_data()
 
     processed_data = process_data(raw_df)
+    
+    no_outlier = teste_outlier(processed_data, gb_test)
+    
+    params, dist_r2 = distributions(
+        no_outlier, table_yn_sigman)
 
-    no_outlier = teste_outlier(processed_data, teste_gb)
+    k_coefficient_data = k_coefficient(params, dist_r2)
 
-    df, params, max_value_r2 = distributions(no_outlier, table_yn_sigman)
+    disaggregation_data, time_interval = disaggregation_coef(params, dist_r2)
+    print(k_coefficient_data["k"])
+    
+    idf_data = ventechow(k_coefficient_data, disaggregation_data, params, time_interval, dist_r2)
 
-    final_data = ventechow(df, params, max_value_r2)
-
-    return final_data
+    # Exportar idf_data para um arquivo JSON
+    with open('node_app/src/idf_data.json', 'w') as f:
+        f.write(idf_data.to_json(orient='records', lines=True))
+        
+    return idf_data
 
 
 if __name__ == "__main__":
