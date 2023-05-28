d837080c1755c49bc86739ce37e5bd6c638465de
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-05-27 21:59:04 -0300

    fix: HTTP request to cloud functions

--- src/app.js
+++ src/app.js
@@ -1,23 +1,13 @@
-import express from "express"
-import cors from "cors"
-import multer from "multer"
-import fs from "fs"
-import path from "path"
-import { fileURLToPath } from "url"
+import express from "express";
+import cors from "cors";
+import multer from "multer";
 import axios from 'axios';
-import bucket from './firebase';
+import firebase from './firebase';
 
 const app = express();
 app.use(cors());
 
-const upload = multer({ dest: 'uploads/' });
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-if (!fs.existsSync('uploads')) {
-  fs.mkdirSync('uploads');
-}
+const upload = multer({ storage: multer.memoryStorage() });
 
 app.post('/upload', upload.single('file'), async(req, res) => {
   if (!req.file) {
@@ -25,18 +15,28 @@ app.post('/upload', upload.single('file'), async(req, res) => {
     return;
   }
 
-  const csvFilePath = req.file.path;
-
-  const functionUrl = 'https://us-central1-calculoidf.cloudfunctions.net/process_request';
+  const csvFilePath = `uploads/${req.file.originalname}`;
 
   try {
-      const response = await axios.post(functionUrl, { csvFilePath });
-      console.log(`Response from Google Cloud Function: ${response.data}`);
-  } catch (error) {
-      console.error(`Error calling Google Cloud Function: ${error}`);
-  }
+    // Upload the file to Firebase Storage
+    await firebase.uploadFile(csvFilePath);
 
-  res.status(200).send({ message: 'Arquivo enviado com sucesso' });
+    const functionUrl = process.env.FUNCTION_URL;
+
+    // Call the Google Cloud Function with the path of the CSV file in Firebase Storage
+    const response = await axios.post(functionUrl, { csvFilePath });
+
+    console.log(`Response from Google Cloud Function: ${response.data}`);
+
+    // Send the JSON received from the Google Cloud Function to the front-end
+    res.status(200).send(response.data);
+
+    // Remove the CSV file from Firebase Storage
+    await firebase.deleteFile(csvFilePath);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    res.status(500).send({ message: 'Ocorreu um erro ao processar o arquivo' });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
