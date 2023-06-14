e4ca8c279bfbf1403b593aa7e136dccb58160aed
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-06-14 14:39:35 -0300

    feat: add error handling into firebase upload

--- src/firebase.cjs
+++ src/firebase.cjs
@@ -12,20 +12,30 @@ admin.initializeApp({
 const bucket = admin.storage().bucket();
 
 async function uploadFile(filePath) {
-  await bucket.upload(filePath, {
-    gzip: true,
-    metadata: {
-      cacheControl: 'public, max-age=31536000',
-    },
-  });
+  try {
+    await bucket.upload(filePath, {
+      gzip: true,
+      metadata: {
+        cacheControl: 'public, max-age=31536000',
+      },
+    });
 
-  console.log(`${filePath} uploaded to Firebase Storage.`);
+    console.log(`${filePath} uploaded to Firebase Storage.`);
+  } catch (error) {
+    console.error(`Error uploading file to Firebase Storage: ${error}`);
+    throw error;
+  }
 }
 
 async function deleteFile(filePath) {
-  await bucket.file(filePath).delete();
+  try {
+    await bucket.file(filePath).delete();
 
-  console.log(`File ${filePath} deleted from Firebase Storage.`);
+    console.log(`File ${filePath} deleted from Firebase Storage.`);
+  } catch (error) {
+    console.error(`Error deleting file from Firebase Storage: ${error}`);
+    throw error;
+  }
 }
 
 module.exports = { uploadFile, deleteFile, bucket };
